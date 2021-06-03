0cc2de771623e58fd1d3e107280257ba707a1416
Omer Abarzel <omer.abarzel@live.biu.ac.il>
2021-06-03 15:55:22 +0300

    home page

---
+++ lib/screens/home.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/light_colors.dart';
+import 'package:flutter_app/widgets/active_project_card.dart';
+import 'package:flutter_app/widgets/task_column.dart';
+import 'package:flutter_app/widgets/top_container.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class HomePage extends StatelessWidget {
+  Text subheading(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          color: LightColors.kDarkBlue,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1.2),
+    );
+  }
+
+  static CircleAvatar calendarIcon() {
+    return CircleAvatar(
+      radius: 25.0,
+      backgroundColor: LightColors.kGreen,
+      child: Icon(
+        Icons.calendar_today,
+        size: 20.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: LightColors.kLightYellow,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            TopContainer(
+              height: 200,
+              width: width,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(Icons.menu,
+                            color: LightColors.kDarkBlue, size: 30.0),
+                        Icon(Icons.search,
+                            color: LightColors.kDarkBlue, size: 25.0),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 0.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  'Hello!',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 50.0,
+                                    color: LightColors.kDarkBlue,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Shimrit Harrai',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ]),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.transparent,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              subheading('To Do:'),
+                            ],
+                          ),
+                          SizedBox(height: 15.0),
+                          TaskColumn(
+                            icon: Icons.alarm,
+                            iconBackgroundColor: LightColors.kRed,
+                            title: 'Asset questionnaire',
+                            subtitle: 'Enter details about the property',
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          TaskColumn(
+                            icon: Icons.blur_circular,
+                            iconBackgroundColor: LightColors.kDarkYellow,
+                            title: 'Scans',
+                            subtitle:
+                                'Let start scanning the contents of the asset',
+                          ),
+                          SizedBox(height: 15.0),
+                          TaskColumn(
+                            icon: Icons.check_circle_outline,
+                            iconBackgroundColor: LightColors.kBlue,
+                            title: 'Special items',
+                            subtitle: 'valuable and special items',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      color: Colors.transparent,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          subheading('Active Projects'),
+                          SizedBox(height: 5.0),
+                          Row(
+                            children: <Widget>[
+                              ActiveProjectsCard(
+                                cardColor: LightColors.kGreen,
+                                loadingPercent: 0.25,
+                                title: 'Medical App',
+                                subtitle: '9 hours progress',
+                              ),
+                              SizedBox(width: 20.0),
+                              ActiveProjectsCard(
+                                cardColor: LightColors.kRed,
+                                loadingPercent: 0.6,
+                                title: 'Making History Notes',
+                                subtitle: '20 hours progress',
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              ActiveProjectsCard(
+                                cardColor: LightColors.kDarkYellow,
+                                loadingPercent: 0.45,
+                                title: 'Sports App',
+                                subtitle: '5 hours progress',
+                              ),
+                              SizedBox(width: 20.0),
+                              ActiveProjectsCard(
+                                cardColor: LightColors.kBlue,
+                                loadingPercent: 0.9,
+                                title: 'Online Flutter Course',
+                                subtitle: '23 hours progress',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
